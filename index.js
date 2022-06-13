29312d8fa3a2d00b60efbca0c77662dbf99c214f
haim9876 <76274413+haim9876@users.noreply.github.com>
2022-06-12 22:17:13 -0400

    Update 3
    Update html, css

---
+++ index.js
@@ -0,0 +1,34 @@
+var count = 0;
+
+function loadJson(){
+    let response = fetch("./test.json")
+    .then(response => {
+        console.log(response);
+        if(response.status == 200)
+        {
+            return response.json();
+        }
+    })
+    .then(jsondata => {
+        console.log(jsondata);
+        count = jsondata.length
+        for(let i = 0; i < count; i++) {
+			var table = document.getElementById("myTable");
+			var row = table.insertRow(i + 1);
+			var cel1 = row.insertCell(0);
+			var cel2 = row.insertCell(1);
+			var cel3 = row.insertCell(2);
+			var cel4 = row.insertCell(3);
+			cel1.innerHTML = jsondata[i]["School"];
+			cel2.innerHTML = jsondata[i]["Program"];
+			cel3.innerHTML = jsondata[i]["Type"];
+			cel4.innerHTML = jsondata[i]["Year"];
+		}
+    });
+}
+
+function clearAction(){
+    for(let i = 0; i < count; i++) {
+    	document.getElementById("myTable").deleteRow(i + 1);
+    }
+}
