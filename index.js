9a31b2ccf1943624e07d443d0f0a88b8c00ab16e
haim9876 <76274413+haim9876@users.noreply.github.com>
2022-06-12 22:55:47 -0400

    update 4
    Update html and js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var count = 0;
 
 function loadJson(){
-    let response = fetch("test.json")
+    let response = fetch("./test.json")
     .then(response => {
         console.log(response);
         if(response.status == 200)
@@ -11,24 +11,27 @@ function loadJson(){
     })
     .then(jsondata => {
         console.log(jsondata);
-        count = jsondata.length
-        for(let i = 0; i < count; i++) {
-			var table = document.getElementById("myTable");
-			var row = table.insertRow(i + 1);
-			var cel1 = row.insertCell(0);
-			var cel2 = row.insertCell(1);
-			var cel3 = row.insertCell(2);
-			var cel4 = row.insertCell(3);
-			cel1.innerHTML = jsondata[i]["School"];
-			cel2.innerHTML = jsondata[i]["Program"];
-			cel3.innerHTML = jsondata[i]["Type"];
-			cel4.innerHTML = jsondata[i]["Year"];
+        if(count == 0) {
+	        count = jsondata.length
+	        for(let i = 0; i < count; i++) {
+				var table = document.getElementById("myTable");
+				var row = table.insertRow(i + 1);
+				var cel1 = row.insertCell(0);
+				var cel2 = row.insertCell(1);
+				var cel3 = row.insertCell(2);
+				var cel4 = row.insertCell(3);
+				cel1.innerHTML = jsondata[i]["School"];
+				cel2.innerHTML = jsondata[i]["Program"];
+				cel3.innerHTML = jsondata[i]["Type"];
+				cel4.innerHTML = jsondata[i]["Year"];
+			}
 		}
     });
 }
 
 function clearAction(){
     for(let i = 0; i < count; i++) {
-    	document.getElementById("myTable").deleteRow(i + 1);
+    	document.getElementById("myTable").deleteRow(1);
     }
+    count = 0;
 }
--- readme.txt
+++ readme.txt
@@ -1,4 +1,14 @@
-1. Create new server in github
-2. Set server url as https://haim9876.github.io/gitpublicdomainfree/
-3. Check send Ajax button
-4. Check clear button
+How to View:
+[1] Open the public URL hosted on github: https://haim9876.github.io/gitpublicdomainfree/
+[2] To view the code either inspect the code on webpage or open up the folder in VS code.
+
+Folder Contents:
+[1] JSON file
+[2] HTML file
+[3] JS file
+[4] CSS file
+
+[1] Create a new server in github.
+[2] Set server url as: https://haim9876.github.io/gitpublicdomainfree/
+[3] Check Get Data Button
+[4] Check Clear Button
